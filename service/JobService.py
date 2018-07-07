7aca060521fce391b50c99906c6d339ef9ada013
AshV <ashish6318@gmail.com>
2018-07-07 19:34:51 +0530

    Job working fine with CORS

--- service/JobService.py
+++ service/JobService.py
@@ -10,14 +10,16 @@ class JobService:
         self.jobDal = JobDal()
 
     def handle_job(self, file_params):
-        file_path = AppUtil.create_file(file_params["file_stream"], file_params["file_name"])
-
-        payload = {
-            "file_path" : file_path
-        }
+        file_path = AppUtil.create_file(file_params["file_stream"],file_params["file_id"] + file_params["file_name"])
 
         # This sleep is just to simulate a call
-        time.sleep(60)
+        time.sleep(5)
+
+        payload = {
+            "status":"processed",
+            "result":'{"Algo1":"50","Algo2":"100"}',
+            "localFilePath" : file_path
+        }
 
         # Post result to firebase
-        # HttpService.send_request("<end_point_path", http_method='PUT', params="params", payload=payload)
+        HttpService.send_request("https://outlaystore.firebaseio.com/files/" + file_params["file_id"] + "/.json", http_method='PATCH', params="params", payload=payload)
